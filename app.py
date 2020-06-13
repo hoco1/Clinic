0b9b3190dc3120aee44b1c62901c838a2e2abda7
mohammadreza norouzi <mnorouzii600@gmail.com>
2020-06-13 12:19:42 +0430

    Register BluePint add sqlalchemy migrate

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Development
 app = Flask(__name__)
+app.config.from_object(Development)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
-import views
+from views import *
 
 from mod_doctor import doctor 
-from mod_operator import operator 
-from mod_sick import sick 
+from mod_operator import operator
+from mod_nurse import nurse 
 from mod_upload import uploads
 
 app.register_blueprint(doctor)
 app.register_blueprint(operator)
-app.register_blueprint(sick)
+app.register_blueprint(nurse)
 app.register_blueprint(uploads)
